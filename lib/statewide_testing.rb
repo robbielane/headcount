ef828f0a224eba9cb0eb4cac16c6e41fc674668e
Robbie Lane <lane.robbie@gmail.com>
2015-09-16 19:16:37 -0600

    Statewide Testing Working and Tested

--- lib/statewide_testing.rb
+++ lib/statewide_testing.rb
@@ -1,5 +1,4 @@
 require_relative 'statewide_testing_loader'
-require 'csv'
 
 class StatewideTesting
   def initialize(name)
@@ -10,8 +9,6 @@ class StatewideTesting
     case grade
     when 3
       data = StatewideTestingLoader.load_proficient_grade_three
-    when 4
-      data = StatewideTestingLoader.load_proficient_grade_four
     when 8
       data = StatewideTestingLoader.load_proficient_grade_eight
     else
@@ -20,7 +17,7 @@ class StatewideTesting
 
     results = data.select { |row| row if row[:location] == @name }
         .group_by { |r| r[:timeframe] }
-        .map { |k,v| {k.to_i => v.map { |k| {k[:score].downcase.to_sym=>k[:data].to_f} } } }
+        .map { |k,v| {k.to_i => v.map { |k| {k[:score].downcase.to_sym=>k[:data][0..4].to_f} } } }
     format_results(results)
   end
 
@@ -28,11 +25,46 @@ class StatewideTesting
     formatted_result = {}
     results.each_with_index do |element, index|
       results[index].each do |k,v|
-        formatted_result[k] = v[0].merge(v[1]).merge(v[2]).sort
+        formatted_result[k] = v[0].merge(v[1]).merge(v[2])
       end
     end
     formatted_result
   end
+
+  def proficient_by_race_or_ethnicity(race)
+    math = StatewideTestingLoader.load_average_math_prof_by_race
+    reading = StatewideTestingLoader.load_average_reading_prof_by_race
+    writing = StatewideTestingLoader.load_average_writing_prof_by_race
+
+    results = Hash.new
+    results[:math] = math.select { |row| row if row[:location] == @name && row[:race_ethnicity].downcase == race.to_s }
+    results[:reading] = reading.select { |row| row if row[:location] == @name && row[:race_ethnicity].downcase == race.to_s }
+    results[:writing] = writing.select { |row| row if row[:location] == @name && row[:race_ethnicity].downcase == race.to_s }
+
+    formatted_results = Hash.new
+
+    results.each do |result_type, result_data|
+      result_data.each do |row|
+        timeframe = row[:timeframe].to_i
+        formatted_results[timeframe] ||= Hash.new
+        formatted_results[timeframe][result_type] = row[:data][0..4].to_f
+      end
+    end
+    formatted_results
+  end
+
+  def proficient_for_subject_by_grade_in_year(subject, grade, year)
+    data = proficient_by_grade(grade)
+    data_for_year = data.select { |k,v| v if k == year}
+    data_for_year[year][subject]
+  end
+
+  def proficient_for_subject_by_race_in_year(subject, race, year)
+    data = proficient_by_race_or_ethnicity(race)
+    data_by_race = data.select { |k,v| v if k == year}
+    data_by_race[year][subject]
+  end
 end
-binding.pry
-StatewideTesting.new('ACADEMY 20').proficient_by_grade(3)
+
+
+StatewideTesting.new('ACADEMY 20').proficient_for_subject_by_race_in_year(:writing, "asian", 2011)
--- lib/statewide_testing_loader.rb
+++ lib/statewide_testing_loader.rb
@@ -20,21 +20,27 @@ class StatewideTestingLoader
               .to_a.map { |row| row.to_hash}
   end
 
-  def load_proficient_grade_three
+  def load_proficiency_data_by_grade_three
     grade_three_prof = "./data/3rd grade students scoring proficient or above on the CSAP_TCAP.csv"
     data = CSV.open(grade_three_prof, {headers: true, header_converters: :symbol})
               .to_a.map { |row| row.to_hash}
   end
 
-  def load_average_math_prof_by_race
-    average_prof_by_race_math = "..data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Math.csv"
-    data = CSV.open(average_prof_by_race_math, {headers: true, header_converters: :symbol})
+  def self.load_average_math_prof_by_race
+    average_prof_by_race_math = "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Math.csv"
+    math = CSV.open(average_prof_by_race_math, {headers: true, header_converters: :symbol})
               .to_a.map { |row| row.to_hash}
   end
 
+  def self.load_average_reading_prof_by_race
+    average_prof_by_race_reading = "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Reading.csv"
+    reading = CSV.open(average_prof_by_race_reading, {headers: true, header_converters: :symbol})
+                 .to_a.map { |row| row.to_hash }
+  end
 
-
-
-    average_prof_by_race_reading = "..data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Reading.csv"
-    average_prof_by_race_writing = "..data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Writing.csv"
+  def self.load_average_writing_prof_by_race
+    average_prof_by_race_writing = "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Writing.csv"
+    writing = CSV.open(average_prof_by_race_writing, {headers: true, header_converters: :symbol})
+                 .to_a.map { |row| row.to_hash }
+  end
 end
