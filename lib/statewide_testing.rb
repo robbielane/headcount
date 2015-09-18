215624f0ebfa177172e47732c71625922cc08eb4
Robbie Lane <lane.robbie@gmail.com>
2015-09-18 09:23:29 -0600

    made UnknownDataError more robust for StatewideTesting

--- lib/statewide_testing.rb
+++ lib/statewide_testing.rb
@@ -4,16 +4,26 @@ class UnknownDataError < StandardError
 end
 
 class StatewideTesting
+  RACES = {asian: "Asian",
+           black: "Black",
+           pacific_islander: "Hawaiian/Pacific Islander",
+           hispanic: "Hispanic",
+           native_american: "Native American",
+           two_or_more: "Two or more",
+           white: "White"}
+
+  SUBJECTS = [:math, :reading, :writing]
+  GRADES = [3, 8]
+
+
   def initialize(name)
     @name = name
   end
 
   def proficient_by_grade(grade)
     case grade
-    when 3
-      data = StatewideTestingLoader.load_proficient_grade_three
-    when 8
-      data = StatewideTestingLoader.load_proficient_grade_eight
+    when 3 then data = StatewideTestingLoader.load_proficient_grade_three
+    when 8 then data = StatewideTestingLoader.load_proficient_grade_eight
     else
       raise UnknownDataError
     end
@@ -35,18 +45,16 @@ class StatewideTesting
   end
 
   def proficient_by_race_or_ethnicity(race)
-    race = race.to_sym
-    races = [:asian, :black, :pacific_islander, :hispanic, :native_american, :two_or_more, :white]
-    raise UnknownDataError if !races.include?(race)
+    raise UnknownDataError unless RACES.keys.include?(race)
 
     math = StatewideTestingLoader.load_average_math_prof_by_race
     reading = StatewideTestingLoader.load_average_reading_prof_by_race
     writing = StatewideTestingLoader.load_average_writing_prof_by_race
 
     results = Hash.new
-    results[:math] = math.select { |row| row if row[:location] == @name && row[:race_ethnicity].downcase == race.to_s }
-    results[:reading] = reading.select { |row| row if row[:location] == @name && row[:race_ethnicity].downcase == race.to_s }
-    results[:writing] = writing.select { |row| row if row[:location] == @name && row[:race_ethnicity].downcase == race.to_s }
+    results[:math] = math.select { |row| row if row[:location] == @name && row[:race_ethnicity] == RACES.fetch(race) }
+    results[:reading] = reading.select { |row| row if row[:location] == @name && row[:race_ethnicity] == RACES.fetch(race) }
+    results[:writing] = writing.select { |row| row if row[:location] == @name && row[:race_ethnicity] == RACES.fetch(race) }
 
     formatted_results = Hash.new
 
@@ -61,12 +69,16 @@ class StatewideTesting
   end
 
   def proficient_for_subject_by_grade_in_year(subject, grade, year)
+    #raise UnknownDataError if year not included in data.
+    raise UnknownDataError unless SUBJECTS.include?(subject) && GRADES.include?(grade)
     data = proficient_by_grade(grade)
     data_for_year = data.select { |k,v| v if k == year}
     data_for_year[year][subject]
   end
 
   def proficient_for_subject_by_race_in_year(subject, race, year)
+    #raise UnknownDataError if year not included in data.
+    raise UnknownDataError unless SUBJECTS.include?(subject) && RACES.keys.include?(race)
     data = proficient_by_race_or_ethnicity(race)
     data_by_race = data.select { |k,v| v if k == year}
     data_by_race[year][subject]
