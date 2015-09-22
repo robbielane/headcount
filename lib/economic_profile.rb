f40e40f37cafccd296b2c55b5a295aa811fa6af3
Robbie Lane <lane.robbie@gmail.com>
2015-09-21 21:08:24 -0600

    Added methods for EconomicProfile - more tests still needed

--- lib/economic_profile.rb
+++ lib/economic_profile.rb
@@ -9,6 +9,60 @@ class EconomicProfile
 
   def free_or_reduced_lunch_by_year
     data = loader.load_students_reduced_priced_lunch
-    binding.pry
+    selected_rows = data.select { |row| row[:location] == @name &&
+                                        row[:poverty_level] == "Eligible for Free or Reduced Lunch" &&
+                                        row[:dataformat] == "Percent" }
+    results = {}
+    selected_rows.each do |row|
+      results[row[:timeframe].to_i] = row[:data][0..4].to_f
+    end
+    return nil if results.empty?
+    results
+  end
+
+  def free_or_reduced_lunch_in_year(year)
+    data = loader.load_students_reduced_priced_lunch
+    selected_rows = data.select { |row| row[:location] == @name &&
+                                        row[:timeframe].to_i == year &&
+                                        row[:poverty_level] == "Eligible for Free or Reduced Lunch" &&
+                                        row[:dataformat] == "Percent" }
+    selected_rows[0][:data][0..4].to_f unless selected_rows.empty?
+  end
+
+  def school_aged_children_in_poverty_by_year
+    data = loader.load_school_aged_children_in_poverty
+    selected_rows = data.select { |row| row[:location] == @name &&
+                                        row[:dataformat] == "Percent"}
+    results = {}
+    selected_rows.each do |row|
+      results[row[:timeframe].to_i] = row[:data][0..4].to_f
+    end
+    results
+  end
+
+  def school_aged_children_in_poverty_in_year(year)
+    data = loader.load_school_aged_children_in_poverty
+    selected_rows = data.select { |row| row[:location] == @name &&
+                                        row[:dataformat] == "Percent" &&
+                                        row[:timeframe].to_i == year}
+    selected_rows[0][:data][0..4].to_f unless selected_rows.empty?
+  end
+
+  def title_1_students_by_year
+    data = loader.load_title_one_students
+    selected_rows = data.select { |row| row[:location] == @name }
+    results = {}
+    selected_rows.each do |row|
+      results[row[:timeframe].to_i] = row[:data][0..4].to_f
+    end
+    return nil if results.empty?
+    results
+  end
+
+  def title_1_students_in_year(year)
+    data = loader.load_title_one_students
+    selected_rows = data.select { |row| row[:location] == @name &&
+                                        row[:timeframe].to_i == year }
+    selected_rows[0][:data][0..4].to_f unless selected_rows.empty?
   end
 end
--- test/economic_profile_test.rb
+++ test/economic_profile_test.rb
@@ -1,4 +1,4 @@
-require_relative 'economic_profile'
+require_relative '../lib/economic_profile'
 
 class EconomicProfileTest < Minitest::Test
   def example
@@ -6,21 +6,65 @@ class EconomicProfileTest < Minitest::Test
   end
 
   def test_free_or_reduced_lunch_by_year_returns_data_from_all_years
-    expected_result = { 2000 => 0.020,
-                        2001 => 0.024,
-                        2002 => 0.027,
-                        2003 => 0.030,
-                        2004 => 0.034,
+    expected_result = { 2000 => 0.040,
+                        2001 => 0.047,
+                        2002 => 0.048,
+                        2003 => 0.060,
+                        2004 => 0.059,
                         2005 => 0.058,
-                        2006 => 0.041,
-                        2007 => 0.050,
-                        2008 => 0.061,
-                        2009 => 0.070,
-                        2010 => 0.079,
-                        2011 => 0.084,
+                        2006 => 0.072,
+                        2007 => 0.080,
+                        2008 => 0.093,
+                        2009 => 0.103,
+                        2010 => 0.113,
+                        2011 => 0.119,
                         2012 => 0.125,
-                        2013 => 0.091,
-                        2014 => 0.087,}
+                        2013 => 0.131,
+                        2014 => 0.127,}
     assert_equal expected_result, example.free_or_reduced_lunch_by_year
+  end
+
+  def test_free_or_reduced_lunch_in_year_returns_data
+    assert_equal 0.058, example.free_or_reduced_lunch_in_year(2005)
+  end
+
+  def test_school_aged_children_in_poverty_by_year
+    expected_result = { 1995 => 0.032,
+                        1997 => 0.035,
+                        1999 => 0.032,
+                        2000 => 0.031,
+                        2001 => 0.029,
+                        2002 => 0.033,
+                        2003 => 0.037,
+                        2004 => 0.034,
+                        2005 => 0.042,
+                        2006 => 0.036,
+                        2007 => 0.039,
+                        2008 => 0.044,
+                        2009 => 0.047,
+                        2010 => 0.057,
+                        2011 => 0.059,
+                        2012 => 0.064,
+                        2013 => 0.048,
+                        }
+    assert_equal expected_result, example.school_aged_children_in_poverty_by_year
+  end
+
+  def test_school_aged_children_in_poverty_in_year
+    assert_equal 0.059, example.school_aged_children_in_poverty_in_year(2011)
+  end
+
+  def test_title_one_students_by_year
+    expected_result = {2009 => 0.014,
+                       2011 => 0.011,
+                       2012 => 0.010,
+                       2013 => 0.012,
+                       2014 => 0.027,
+                     }
+    assert_equal expected_result, example.title_1_students_by_year
+  end
+
+  def test_title_one_students_in_year
+    assert_equal 0.011, example.title_1_students_in_year(2011)
   end
 end
