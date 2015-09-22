63c59a207a3ef25b2be9e96dc6884c98ab42d6ad
Ryan Johnson <rjohnson4444@live.com>
2015-09-21 23:38:22 -0600

    Did some light refacotring in economic profile

--- lib/economic_profile.rb
+++ lib/economic_profile.rb
@@ -7,11 +7,7 @@ class EconomicProfile
     @loader = loader
   end
 
-  def free_or_reduced_lunch_by_year
-    data = loader.load_students_reduced_priced_lunch
-    selected_rows = data.select { |row| row[:location] == @name &&
-                                        row[:poverty_level] == "Eligible for Free or Reduced Lunch" &&
-                                        row[:dataformat] == "Percent" }
+  def formatted_data_by_year_and_data(selected_rows)
     results = {}
     selected_rows.each do |row|
       results[row[:timeframe].to_i] = row[:data][0..4].to_f
@@ -20,19 +16,34 @@ class EconomicProfile
     results
   end
 
+  def formatted_data_based_on_yearf_unless_empty(selected_rows)
+    selected_rows[0][:data][0..4].to_f unless selected_rows.empty?
+  end
+
+  def free_or_reduced_lunch_by_year
+    data = loader.load_students_reduced_priced_lunch
+    selected_rows = data.select { |row| row[:location] == @name &&
+                                        row[:poverty_level] == "Eligible for Free or Reduced Lunch" &&
+                                        row[:dataformat] == "Percent" }
+
+    formatted_data_by_year_and_data(selected_rows)
+  end
+
   def free_or_reduced_lunch_in_year(year)
     data = loader.load_students_reduced_priced_lunch
     selected_rows = data.select { |row| row[:location] == @name &&
                                         row[:timeframe].to_i == year &&
                                         row[:poverty_level] == "Eligible for Free or Reduced Lunch" &&
                                         row[:dataformat] == "Percent" }
-    selected_rows[0][:data][0..4].to_f unless selected_rows.empty?
+
+    formatted_data_based_on_yearf_unless_empty(selected_rows)
   end
 
   def school_aged_children_in_poverty_by_year
     data = loader.load_school_aged_children_in_poverty
     selected_rows = data.select { |row| row[:location] == @name &&
                                         row[:dataformat] == "Percent"}
+
     results = {}
     selected_rows.each do |row|
       results[row[:timeframe].to_i] = row[:data][0..4].to_f
@@ -45,24 +56,22 @@ class EconomicProfile
     selected_rows = data.select { |row| row[:location] == @name &&
                                         row[:dataformat] == "Percent" &&
                                         row[:timeframe].to_i == year}
-    selected_rows[0][:data][0..4].to_f unless selected_rows.empty?
+
+    formatted_data_based_on_yearf_unless_empty(selected_rows)
   end
 
   def title_1_students_by_year
     data = loader.load_title_one_students
     selected_rows = data.select { |row| row[:location] == @name }
-    results = {}
-    selected_rows.each do |row|
-      results[row[:timeframe].to_i] = row[:data][0..4].to_f
-    end
-    return nil if results.empty?
-    results
+
+    formatted_data_by_year_and_data(selected_rows)
   end
 
   def title_1_students_in_year(year)
     data = loader.load_title_one_students
     selected_rows = data.select { |row| row[:location] == @name &&
                                         row[:timeframe].to_i == year }
-    selected_rows[0][:data][0..4].to_f unless selected_rows.empty?
+
+    formatted_data_based_on_yearf_unless_empty(selected_rows)
   end
 end
