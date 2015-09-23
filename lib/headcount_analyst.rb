5083ee99e6d31e898f9c17264ef228d8af7ed3b3
Robbie Lane <lane.robbie@gmail.com>
2015-09-23 14:16:44 -0600

    HeadcountAnalyst shit

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -4,28 +4,36 @@ class HeadcountAnalyst
   attr_reader :all_districts, :repository
   def initialize(repo)
     @repository = repo
-    @statewide_testing_results = {:math=>{},:writing=>{},:reading=>{} }
+    @statewide_testing_results ||= {:math=>{},:writing=>{},:reading=>{} }
     @all_districts = @repository.find_all_districts
   end
 
-  def top_statewide_testing_year_over_year_growth_in_3rd_grade(subject)
+  def top_statewide_testing_year_over_year_growth_in_3rd_grade(args)
+    subject = args.fetch(:subject)
+    number_of_results = 1
+    number_of_results = args.fetch(:top) if args.keys.include?(:top)
     data = retrieve_results(subject)
     data = data.map { |district, scores| [district, ((scores.values.max - scores.values.min) / (scores.keys.max - scores.keys.min))] if !scores.empty? }.compact
                .select { |district| !district[1].nan? }
-               .max_by { |district| district[1] }
-    data = [data[0], data[1].to_s[0..4].to_f]
+               .max_by(number_of_results) { |district| district[1] }
+
+    data = data.map { |row| [row[0], row[1].to_s[0..4].to_f] }
+    data = data.flatten if number_of_results == 1
+    data
   end
 
 
   def retrieve_results(subject)
-    hash1 = {}
-    all_districts.each do |district|
-      data_for_district = district.statewide_testing.proficient_by_grade(3)
-      hash2 = {}
-      data_for_district.each do |row|
-        hash2[row[0]] = row[1].fetch(subject) if row[1].keys.include?(subject)
+    @statewide_testing_results[subject] = begin
+      hash1 = {}
+      all_districts.each do |district|
+        data_for_district = district.statewide_testing.proficient_by_grade(3)
+        hash2 = {}
+        data_for_district.each do |row|
+          hash2[row[0]] = row[1].fetch(subject) if row[1].keys.include?(subject)
+        end
+        hash1[district.name] = hash2
       end
-      hash1[district.name] = hash2
     end
     @statewide_testing_results[subject] = hash1
   end
---
+++ test/headcount_analyst_test.rb
@@ -0,0 +1,25 @@
+require_relative '../lib/headcount_analyst'
+
+class HeadcountAnalystTest < Minitest::Test
+  def repo
+    repo ||= DistrictRepository.from_csv('./data')
+  end
+
+  def ha
+    ha = HeadcountAnalyst.new(repo)
+  end
+
+  def test_where_the_most_growth_is_happening_state_wide
+    assert_equal ["WILEY RE-13 JT", 0.300],
+      ha.top_statewide_testing_year_over_year_growth_in_3rd_grade(:subject => :math)
+  end
+
+  def test_where_the_most_growth_is_happening_state_wide_top_3
+    expected = [["WILEY RE-13 JT", 0.300], ["LA VETA RE-2", 0.162],
+                  ["LAKE COUNTY R-1", 0.112]]
+    actual = ha.top_statewide_testing_year_over_year_growth_in_3rd_grade(top: 3, subject: :math)
+
+    assert_equal expected, actual
+  end
+
+end
